a58985c0a3ea71c31cd5a34fdfaba622f7ac1600
BotZeus <77181929+BotZeus@users.noreply.github.com>
2021-01-26 13:23:14 -0300

    Add files via upload

---
+++ database/menu/meme.js
@@ -0,0 +1,21 @@
+const meme = (prefix, botName, ownerName) => {
+	return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo`
+}
+exports.meme = meme
+
